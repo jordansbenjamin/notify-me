f14d38ae1ca78030484b83cea36f5e6bb89f7916
jordansbenjamin <14327@coderacademy.edu.au>
2024-01-25 15:56:02 +1100

    implemented & tested requests and beautiful soup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,24 @@
-print("Hello World!")
+import requests
+from bs4 import BeautifulSoup
+
+URL_9024 = 'https://timetable.unsw.edu.au/2024/COMP9024.html'
+
+res = requests.get(URL_9024)
+# print(res.text)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+# print(soup.prettify())
+status_data = soup.find_all('font')
+# print(status_data)
+
+parsed_status = []
+
+for data in status_data:
+  if 'Open' in data or 'Full' in data:
+    parsed_status.append(data)
+
+print(parsed_status)
+
+# save parsed data into csv to compare previous version with updated version
+# figure out time intervals (5 mins) to request page info
+# if there are changes (new differs from previous) then send txt message to notify
