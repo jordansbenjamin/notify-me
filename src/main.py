7ea57025bba409efa5721cb1d9fe5880d9f362cc
jordansbenjamin <14327@coderacademy.edu.au>
2024-01-25 18:41:05 +1100

    implemented & tested csv

--- src/main.py
+++ src/main.py
@@ -1,24 +1,61 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 URL_9024 = 'https://timetable.unsw.edu.au/2024/COMP9024.html'
 
+# response object
 res = requests.get(URL_9024)
-# print(res.text)
-
+# soup obj
 soup = BeautifulSoup(res.text, 'html.parser')
-# print(soup.prettify())
-status_data = soup.find_all('font')
-# print(status_data)
 
-parsed_status = []
+def parse_data():
+  parsed_status = []
+  status_data = soup.find_all('font')
 
-for data in status_data:
-  if 'Open' in data or 'Full' in data:
-    parsed_status.append(data)
+  for data in status_data:
+    if 'Open' in data or 'Full' in data:
+      parsed_status.append(data.text)
 
-print(parsed_status)
+  return parsed_status
+
+# print(type(parsed_status))
 
 # save parsed data into csv to compare previous version with updated version
-# figure out time intervals (5 mins) to request page info
-# if there are changes (new differs from previous) then send txt message to notify
+csv_data = None;
+
+try:
+  with open('status_data.csv') as status_csvf:
+    # reader = csv.reader(status_csvf)
+    # csv_data.append(reader)
+    csv_data = status_csvf.readlines()
+except:
+  with open('status_data.csv', 'w') as status_csvf:
+    writer = csv.writer(status_csvf)
+    writer.writerow(parse_data())
+
+new_csv_data = None
+
+try:
+  with open('new_status_data.csv') as status_csvf:
+    # reader = csv.reader(status_csvf)
+    # csv_data.append(reader)
+    new_csv_data = status_csvf.readlines()
+except:
+  with open('new_status_data.csv', 'w') as status_csvf:
+    writer = csv.writer(status_csvf)
+    writer.writerow(parse_data())
+
+def format_csv(data):
+  if data:
+    # print(csv_data)
+    formatted_csv_data = data[0].split(',')
+    return formatted_csv_data
+
+print(len(format_csv(csv_data)))
+print(len(format_csv(new_csv_data)))
+
+# figure out time intervals (10 mins) to request again
+# if there are changes (new differs from previous) then send txt message to notify (test txt msg first)
+# figure out how to listen to multiple requests and send txt msg for each timetable
+# after all is done, figure out how to run the script 24/7
